6e56cdd86106dde956ee8104f7a977aa0fb74832
denishm116 <denishm116@yandex.ru>
2020-11-21 12:11:13 +0300

    Деплой без метрики (3)

--- components/frontend/Map.vue
+++ components/frontend/Map.vue
@@ -8,61 +8,18 @@
         <client-only>
           <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A398fee05586ef392323c65b0df663620e412c6a89c89f134b449ffa25aa1a938&amp;source=constructor" width="1251" height="439" frameborder="0"></iframe>
         </client-only>
-<!--        <yandex-map-->
-<!--          :coords="coords"-->
-<!--          v-if="isMounted"-->
-<!--          class="ymap-class"-->
-
-<!--        >-->
-<!--          <ymap-marker-->
-<!--            :coords="coords"-->
-<!--            marker-id="landing"-->
-<!--            :icon="markerIcon"-->
-<!--          />-->
-<!--        </yandex-map>-->
       </div>
     </div>
   </section>
 </template>
 <script>
-import {yandexMap, ymapMarker} from 'vue-yandex-maps'
 
 export default {
-  components: {yandexMap, ymapMarker},
-
-  data() {
-    return {
-      isMounted: false,
-      coords: [
-        45.057982,
-        39.007500
-      ],
-      markerIcon: {
-        layout: 'default#imageWithContent',
-        imageHref: "https://potolkov.net/img/logo.png",
-        imageSize: [113, 33],
-        imageOffset: [10, -60],
-        content: 'ул. 40-летия Победы, 33/6',
-        contentOffset: [-30, -35],
-        contentLayout: '<div style="background: red; width: 170px; color: #FFFFFF; font-weight: bold; padding: 3px; border-radius: 3px; opacity: 0.5">$[properties.iconContent]</div>'
-      },
-    }
-  },
-  async mounted() {
-    this.isMounted = true
-  }
 
 }
 </script>
 
 <style>
-/*.ymap-container {*/
-/*  height: 500px;*/
-/*}*/
-.ymap-class {
-  height: 60vh;
-}
-
 #map {
   width: 1000px;
   height: 500px;
